4cb42bed1be8a067aa01d81b7c77e7a8e841fb05
stefano <dipietro.stefano@gmail.com>
2018-09-04 23:23:52 +0200

    Test functions implemented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Those functions have the porpuse of calculate the inverse of
+## a matrix and chche the result to avoid recalculation
+## 
 
-## Write a short comment describing this function
+## This function saves the provided matrix into the cache and returns a list 
+## of functions to save and load the matrix and its inverse from the cache
+##
+## Args:
+##  x: An invertible matrix. Default is the result of matrix()
+##
+## Returns:
+##  A list of four functions:
+##      get() : returns the matrix stored into the cache
+##      set(x) : saves the matrix into the cache
+##      getinverse() : returns the inverse of the matrix (eventually loading it from the cache)
+##      setinverse(i) : sets the inverse of the matrix into the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(i) inverse <<- i
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the matrix stored into the cache.
+## If the inverse was already calculated it will be read from the cache
+##
+## Args:
+##      x : a list containing the four function returned from makeCacheMatrix
+##      ... : further arguments passed to or from other methods
+##
+## Returns
+##  The inverse of the matrix stored into the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("Getting cached data")
+        return(inverse)
+    }
+    matrix <- x$get()
+    inverse <- solve(matrix, ...)
+    x$setinverse(inverse)
+    inverse
 }
